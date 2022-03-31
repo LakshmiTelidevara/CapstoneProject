76aa8858151ab8a8577aa52fb0ff37bbeefb869a
LakshmiTelidevara <anjulaxmirao@gmail.com>
2022-03-31 00:52:31 -0500

    .

---
+++ frontend/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4dj86sbCkk1P3H5d7',
+        issuer: 'https://dev-3502588.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  //luv2shopApiUrl: "https://localhost:8443/api",
+  luv2shopApiUrl: "http://localhost:8080/api",
+  stripePublishableKey: "pk_test_51KgIXKIDegE88jAnlwbFkEkgiKUyTEqcXogzmDPd0hfOqwYa3uumUYx402y7NZiqWdTTgOL0lljqVhKDhoHkCmB40001OwVRmb"
+};
